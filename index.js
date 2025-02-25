5c163718c1661a269870f9b6cdde06d2749bba44
PashaNotebook <p@korshunov.me>
2025-02-25 18:56:45 +0500

    3 stars

--- index.js
+++ index.js
@@ -3,22 +3,40 @@ const ZERO = 'O';
 const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
+const sizeCont = document.getElementById('size');
+const ai = document.getElementById('ai');
+const big = document.getElementById('big');
 
 let field = [];
+let ifBig = false;
+let ifAi = false;
 let currentPlayer = CROSS;
 let gameOver = false;
+let size = 3;
+let total = 0;
 
 startGame();
 addResetListener();
 
+function createField() {
+    total = 0;
+    ifAi = ai.checked;
+    ifBig = big.checked;
+    size = Number(sizeCont.value) || 3;
+    field = [];
+    currentPlayer = CROSS;
+    gameOver = false;
+    for (let i = 0; i < size; i++)
+        field.push(Array(size).fill(EMPTY));
+}
+
 function startGame() {
-    field = Array.from({ length: 3 }, () => Array(3).fill(EMPTY));
-    renderGrid(3);
+    createField();
+    renderGrid(size);
 }
 
 function renderGrid(dimension) {
-    container.innerHTML = '';
-
+    container.innerHTML = '';   
     for (let i = 0; i < dimension; i++) {
         const row = document.createElement('tr');
         for (let j = 0; j < dimension; j++) {
@@ -33,29 +51,47 @@ function renderGrid(dimension) {
 
 function cellClickHandler(row, col) {
     if (gameOver || field[row][col] !== EMPTY) return;
-
+    total++;
     field[row][col] = currentPlayer;
     renderSymbolInCell(currentPlayer, row, col);
-
     if (checkWin(currentPlayer)) {
         gameOver = true;
         alert(`Победил ${currentPlayer}`);
         highlightWinningCells(currentPlayer);
         return;
     }
-
     if (checkDraw()) {
         gameOver = true;
         alert("Победила дружба");
         return;
     }
-
     currentPlayer = currentPlayer === CROSS ? ZERO : CROSS;
+
+    if (big && total > size * size / 2) {
+        for (let i = 0; i < size; i++) {
+            field[i].unshift(EMPTY);
+            field[i].push(EMPTY);
+        }
+        field.unshift(Array(size + 2).fill(EMPTY))
+        field.push(Array(size + 2).fill(EMPTY))
+        size += 2;
+        renderGrid(size);
+    }
+
+    if (currentPlayer == ZERO && ifAi) {
+        while (true) {
+            let x = getRandomInt(size);
+            let y = getRandomInt(size);
+            if (field[x][y] === EMPTY) {
+                cellClickHandler(x,y);
+                break;
+            }
+        }
+    }
 }
 
 function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
-
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
@@ -67,14 +103,7 @@ function findCell(row, col) {
 
 function addResetListener() {
     const resetButton = document.getElementById('reset');
-    resetButton.addEventListener('click', resetClickHandler);
-}
-
-function resetClickHandler() {
-    field = Array.from({ length: 3 }, () => Array(3).fill(EMPTY));
-    currentPlayer = CROSS;
-    gameOver = false;
-    renderGrid(3);
+    resetButton.addEventListener('click', startGame);
 }
 
 function checkWin(player) {
@@ -82,10 +111,8 @@ function checkWin(player) {
         if (field[i].every(cell => cell === player)) return true;
         if (field.every(row => row[i] === player)) return true;
     }
-
     if (field.every((row, index) => row[index] === player)) return true;
     if (field.every((row, index) => row[field.length - 1 - index] === player)) return true;
-
     return false;
 }
 
@@ -98,7 +125,6 @@ function highlightWinningCells(player) {
             return;
         }
     }
-
     for (let j = 0; j < field.length; j++) {
         if (field.every(row => row[j] === player)) {
             for (let i = 0; i < field.length; i++) {
@@ -107,14 +133,12 @@ function highlightWinningCells(player) {
             return;
         }
     }
-
     if (field.every((row, index) => row[index] === player)) {
         for (let i = 0; i < field.length; i++) {
             renderSymbolInCell(player, i, i, 'red');
         }
         return;
     }
-
     if (field.every((row, index) => row[field.length - 1 - index] === player)) {
         for (let i = 0; i < field.length; i++) {
             renderSymbolInCell(player, i, field.length - 1 - i, 'red');
@@ -125,6 +149,10 @@ function highlightWinningCells(player) {
 
 function checkDraw() {
     return field.every(row => row.every(cell => cell !== EMPTY));
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
 /* Test Function */
@@ -151,8 +179,4 @@ function testDraw() {
     clickOnCell(0, 1);
     clickOnCell(2, 1);
     clickOnCell(2, 2);
-}
-
-function clickOnCell(row, col) {
-    findCell(row, col).click();
 }
